4a9f7c89977d20302decc36938843ed636558574
victor.espinellinunes <victorespinellinunes@hotmail.com>
2022-09-13 20:30:58 -0300

    ok

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Abre up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="teste"/>
     </View>
   );
 }
